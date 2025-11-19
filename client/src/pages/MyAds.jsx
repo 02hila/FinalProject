1bb783d00733aa33a996ab285ea9b25556a5866f
02hila <hilam868@gmail.com>
2025-11-19 13:55:35 +0200

    Fix

--- client/src/pages/MyAds.jsx
+++ client/src/pages/MyAds.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useAuth } from "../context/AuthContext";
 import { useNavigate } from "react-router-dom";
 import PageSelectorModal from "../components/PageSelectorModal";
+import "./MyAds.css"; // <--- זה החלק החשוב שמחבר את העיצוב
 
 const MyAds = () => {
   const { user } = useAuth();
